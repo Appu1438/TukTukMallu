b024bd1755424b7e8fbd5f0562c69fcc0c7d6f4a
Appu1438 <139608849+Appu1438@users.noreply.github.com>
2023-10-31 13:19:10 +0530

    Add files via upload

---
+++ config/collection.js
@@ -0,0 +1,10 @@
+module.exports={
+
+    ADMIN_COLLECTION:'admin',
+    USERS_COLLECTION:'users',
+    PROFILE_COLLECTION:'profile',
+    PENDING_COLLECTION:'pending',
+    VEHICLE_COLLECTION:'vehicle',
+    COUNTS_COLLECTION:'count'
+    
+}
